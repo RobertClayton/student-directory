a246cc124ec8e5708355b0033c137861f2da1ea2
Robert Clayton <robbie.c.clayton@gmail.com>
2017-10-09 18:56:13 +0100

    Exersise 6 complete

--- directory.rb
+++ directory.rb
@@ -6,7 +6,7 @@ def input_students
   students = []
   name = gets.chomp
   while !name.empty? do
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: :november, hobbies: "default", country_of_birth: "default", height: "default"}
     puts "now we have #{students.count} students"
     name = gets.chomp
   end
@@ -19,20 +19,38 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  #Q1. print a number before the name:  .each_with_index
+  @students.each_with_index do |students, index|
+    #Q2. only print the students whose name begins with a specific letter:
+    #puts "#{index}. #{students[:name]} (#{students[:cohort]} cohort)" if students[:name][0] == "a"
+
+    #Q3. only print the students whose name is shorter than 12 characters:
+    #puts "#{index}. #{students[:name]} (#{students[:cohort]} cohort)" if students[:name].length < 12
+
+    #from original code:
+    #puts "#{index}. #{students[:name]} (#{students[:cohort]} cohort)"
+  end
+
+  #Q4. prints all students using while or until: made students => @students
+  q4_loop = 0
+  until q4_loop == @students.length
+    #puts "#{students[:name]} (#{students[:cohort]} cohort)"
+    puts "#{q4_loop+1}. #{@students[q4_loop][:name]} (#{@students[q4_loop][:cohort]} cohort)"#Q6 = .center(40)
+    q4_loop += 1
   end
 end
 
+
 def print_footer(students)
-  puts "Overall, we have #{students.length} great students"
+  puts "Overall, we have #{@students.length} great students"
 end
 
 
-students = input_students
+@students = input_students
 print_header
-print(students)
-print_footer(students)
+print(@students)
+print_footer(@students)
+
 
 
 =begin
